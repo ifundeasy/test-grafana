b38931500ddba58881995337b705a422ca71fb85
Afa <ifundeasy@edot.id>
2025-06-17 23:28:14 +0700

    inject service to cloud

--- production/product-one.sh
+++ production/product-one.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-NAMESPACE="product-one"
+NAMESPACE="grafana"
 ALLOY_DIR=~/Developer/test-grafana/production/product-one/0-alloy
 SIMPLE_CRUD_DIR=~/Developer/test-grafana/production/product-one
 
@@ -8,19 +8,12 @@ if [ "$1" == "install" ]; then
   echo "[+] Creating namespace: $NAMESPACE (if not exists)"
   kubectl create namespace "$NAMESPACE" || true
   echo ""
-  echo "[+] Deploying Grafana Alloy for product-one..."
-  cd "$ALLOY_DIR" || exit 1
-  helm upgrade --install --values values.yaml al-product-one grafana/alloy --namespace "$NAMESPACE"
-  echo ""
   echo "[+] Deploying simple-crud app..."
   cd "$SIMPLE_CRUD_DIR" || exit 1
   kubectl apply -f simple-crud --namespace "$NAMESPACE"
   echo ""
   echo "[✓] Alloy + simple-crud deployed in namespace '$NAMESPACE'."
 elif [ "$1" == "uninstall" ]; then
-  echo "[x] Uninstalling Grafana Alloy for product-one..."
-  helm uninstall al-product-one --namespace "$NAMESPACE"
-  echo ""
   echo "[x] Deleting simple-crud app..."
   cd "$SIMPLE_CRUD_DIR" || exit 1
   kubectl delete -f simple-crud --namespace "$NAMESPACE" --ignore-not-found
